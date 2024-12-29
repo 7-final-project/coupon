7fd9a48933aac5307443de03da58ae1cab77dcef
jihyeChu <09strawberry@naver.com>
2024-12-29 12:41:52 +0900

    [ feat ] 쿠폰 수정 응답 DTO V1 생성 ( #1 )

---
+++ src/main/java/com/qring/coupon/application/v1/res/CouponPutResDTOV1.java
@@ -0,0 +1,55 @@
+package com.qring.coupon.application.v1.res;
+
+import com.qring.coupon.domain.model.CouponEntity;
+import com.qring.coupon.domain.model.constraint.CouponStatus;
+import com.qring.coupon.domain.model.constraint.StockStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CouponPutResDTOV1 {
+
+    private CouponDTO couponDTO;
+
+    public static CouponPutResDTOV1 of(CouponEntity couponEntity) {
+        return CouponPutResDTOV1.builder()
+                .couponDTO(CouponDTO.from(couponEntity))
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class CouponDTO{
+        private Long id;
+        private String name;
+        private int totalQuantity;
+        private int remainQuantity;
+        private LocalDateTime openAt;
+        private LocalDateTime expireAt;
+        private StockStatus stockStatus;
+        private CouponStatus couponStatus;
+
+        public static CouponDTO from(CouponEntity couponEntity){
+            return CouponDTO.builder()
+                    .id(couponEntity.getId())
+                    .name(couponEntity.getName())
+                    .totalQuantity(couponEntity.getTotalQuantity())
+                    .remainQuantity(couponEntity.getRemainQuantity())
+                    .openAt(couponEntity.getOpenAt())
+                    .expireAt(couponEntity.getExpiredAt())
+                    .stockStatus(couponEntity.getStockStatus())
+                    .couponStatus(couponEntity.getCouponStatus())
+                    .build();
+        }
+    }
+
+}

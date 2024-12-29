be3f102d611c59c054b4259565a91d7990350d08
jihyeChu <09strawberry@naver.com>
2024-12-29 12:38:37 +0900

    [ feat ] 쿠폰 수정 요청 DTO V1 생성 ( #1 )

---
+++ src/main/java/com/qring/coupon/presentation/v1/req/PutCouponReqDTOV1.java
@@ -0,0 +1,34 @@
+package com.qring.coupon.presentation.v1.req;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class PutCouponReqDTOV1 {
+
+    @Valid
+    @NotNull(message = "쿠폰 정보를 입력해주세요.")
+    private CouponDTO couponDTO;
+
+    private static class CouponDTO {
+        @NotBlank(message = "쿠폰 이름을 입력해주세요.")
+        private String name;
+
+        private int totalQuantity;
+
+        private int remainQuantity;
+
+        @NotNull(message = "쿠폰 오픈 시간을 입력해주세요.")
+        private LocalDateTime openAt;
+
+        @NotNull(message = "쿠폰 만료 일자를 입력해주세요.")
+        private LocalDateTime expiredAt;
+    }
+
+}

52faa1224eca5a51f8646ce53e78d0e41d92fb3e
jihyeChu <09strawberry@naver.com>
2024-12-30 21:59:29 +0900

    [ feat ] CouponStatus 이넘 클래스 형태 변경  ( #1 )

--- src/main/java/com/qring/coupon/domain/model/constraint/CouponStatus.java
+++ src/main/java/com/qring/coupon/domain/model/constraint/CouponStatus.java
@@ -1,7 +1,21 @@
 package com.qring.coupon.domain.model.constraint;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum CouponStatus {
 
-    INACTIVE, ACTIVE, CLOSED
+    INACTIVE(Status.INACTIVE),
+    ACTIVE(Status.ACTIVE),
+    CLOSED(Status.CLOSED);
 
+    private final String status;
+
+    public static final class Status {
+        public static final String INACTIVE = "비활성";
+        public static final String ACTIVE = "활성";
+        public static final String CLOSED = "종료";
+    }
 }

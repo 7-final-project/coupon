b79b9af85437218aacadf5530a56e85222e82453
jihyeChu <09strawberry@naver.com>
2024-12-30 22:02:16 +0900

    [ feat ] StockStatus 이넘 클래스 형태 변경  ( #1 )

--- src/main/java/com/qring/coupon/domain/model/constraint/StockStatus.java
+++ src/main/java/com/qring/coupon/domain/model/constraint/StockStatus.java
@@ -1,5 +1,18 @@
 package com.qring.coupon.domain.model.constraint;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum StockStatus {
-    IN_STOCK, OUT_OF_STOCK
+    IN_STOCK(Status.IN_STOCK), // 재고있음
+    OUT_OF_STOCK(Status.OUT_OF_STOCK); // 품절
+
+    private final String status;
+
+    public static class Status {
+        public static final String IN_STOCK = "품절";
+        public static final String OUT_OF_STOCK = "재고있음";
+    }
 }

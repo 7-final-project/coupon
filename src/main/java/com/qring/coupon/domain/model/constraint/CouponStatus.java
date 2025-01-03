04f6fd9010c2d306ea0db3b81adbdc23208313ba
jihyeChu <09strawberry@naver.com>
2025-01-04 01:34:00 +0900

    [ feat ] CouponStatus, IssuanceStatus 이넘 클래스 fromString 메서드 추가 ( #8 )

--- src/main/java/com/qring/coupon/domain/model/constraint/CouponStatus.java
+++ src/main/java/com/qring/coupon/domain/model/constraint/CouponStatus.java
@@ -18,4 +18,13 @@ public enum CouponStatus {
         public static final String ACTIVE = "활성";
         public static final String EXPIRED = "만료";
     }
+
+    public static CouponStatus fromString(String status) {
+        return switch (status) {
+            case Status.INACTIVE -> CouponStatus.INACTIVE;
+            case Status.ACTIVE -> CouponStatus.ACTIVE;
+            case Status.EXPIRED -> CouponStatus.EXPIRED;
+            default -> throw new IllegalArgumentException("유효하지 않은 쿠폰 상태입니다: " + status);
+        };
+    }
 }
--- src/main/java/com/qring/coupon/domain/model/constraint/IssuanceStatus.java
+++ src/main/java/com/qring/coupon/domain/model/constraint/IssuanceStatus.java
@@ -16,4 +16,12 @@ public enum IssuanceStatus {
         public static final String OPEN = "개시";
         public static final String CLOSED = "종료";
     }
+
+    public static IssuanceStatus fromString(String status) {
+        return switch (status) {
+            case Status.OPEN -> IssuanceStatus.OPEN;
+            case Status.CLOSED -> IssuanceStatus.CLOSED;
+            default -> throw new IllegalArgumentException("유효하지 않은 쿠폰 발급 상태입니다." + status);
+        };
+    }
 }

c514c3341c0df0c537c5365d76de37af2d8628b1
jihyeChu <09strawberry@naver.com>
2025-01-06 19:34:35 +0900

    [ feat ] 메서드 이름 변경 : validateUserRole ( #10 )

--- src/main/java/com/qring/coupon/application/v1/service/CouponServiceV1.java
+++ src/main/java/com/qring/coupon/application/v1/service/CouponServiceV1.java
@@ -88,7 +88,7 @@ public class CouponServiceV1 {
 
         CouponEntity couponEntityForModification = getCouponEntityById(id);
 
-        validateMasterRole(passport);
+        validateUserRole(passport);
 
         validateCouponNameDuplicate(id, dto.getCoupon().getName());
 
@@ -123,7 +123,7 @@ public class CouponServiceV1 {
     // -----
     // NOTE : 쿠폰 생성 검증 프로세스
     private void validateCouponCreationProcess(String passport, PostCouponReqDTOV1 dto) {
-        validateMasterRole(passport);
+        validateUserRole(passport);
 
         validateCouponNameDuplicate(dto.getCoupon().getName());
 
@@ -140,7 +140,7 @@ public class CouponServiceV1 {
 
     // -----
     // NOTE : 관리자 권한 검증
-    private void validateMasterRole(String passport) {
+    private void validateUserRole(String passport) {
         if (!PassportUtil.getRole(passport).equals("관리자")) {
             throw new UnauthorizedAccessException("쿠폰 생성 권한이 없습니다.");
         }

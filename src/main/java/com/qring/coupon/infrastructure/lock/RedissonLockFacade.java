6de61ab99a4bdbec192a362cd69f493da09cf37c
jihyeChu <09strawberry@naver.com>
2025-01-09 19:15:09 +0900

    [ feat ] 레디슨을 사용한 쿠폰 발급 분산락 추가 ( #14 )

---
+++ src/main/java/com/qring/coupon/infrastructure/lock/RedissonLockFacade.java
@@ -0,0 +1,41 @@
+package com.qring.coupon.infrastructure.lock;
+
+import com.qring.coupon.application.v1.res.CouponPostByIdResDTOV1;
+import com.qring.coupon.application.v1.service.CouponServiceV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j(topic = "redisson-lock")
+@Component
+@RequiredArgsConstructor
+public class RedissonLockFacade {
+
+    private final RedissonClient redissonClient;
+
+    private final CouponServiceV1 couponServiceV1;
+
+    public CouponPostByIdResDTOV1 issueCouponWithLockById(Long userId, Long id, String username) {
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                log.info("Lock 획득 실패");
+                return CouponPostByIdResDTOV1.of(null);
+            }
+
+            return couponServiceV1.issueBy(userId, id, username);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}

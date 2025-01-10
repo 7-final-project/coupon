9be224832bb7d3bb8c920d0b09d56b3172b3876e
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-10 16:10:47 +0900

    [ feat ] 낙관적 락 처리를 위한 Coupon 발급 퍼사드 패턴 적용 ( #15 )

---
+++ src/main/java/com/qring/coupon/infrastructure/lock/OptimisticLockFacade.java
@@ -0,0 +1,35 @@
+package com.qring.coupon.infrastructure.lock;
+
+import com.qring.coupon.application.v1.res.CouponPostByIdResDTOV1;
+import com.qring.coupon.application.v1.service.CouponServiceV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j(topic = "OptimisticLockFacade")
+public class OptimisticLockFacade {
+
+    private final CouponServiceV1 couponServiceV1;
+
+    public CouponPostByIdResDTOV1 issueCouponByIdWithOptimisticLock(Long userId, Long id, String username) {
+        int retryCount = 5; // 최대 재시도 횟수
+        while (retryCount-- > 0) {
+            try {
+                return couponServiceV1.issueBy(userId, id, username);
+            } catch (IllegalArgumentException e) {
+                throw e;
+            } catch (Exception e) {
+                log.warn("Version 불일치 발생! : {}", e.getMessage());
+                try {
+                    Thread.sleep(50); // 재시도 전 잠시 대기
+                } catch (InterruptedException interruptedException) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException("스레드 실행이 중단되었습니다.", interruptedException);
+                }
+            }
+        }
+        throw new RuntimeException("쿠폰 발급 재시도 최대 횟수를 초과했습니다.");
+    }
+}

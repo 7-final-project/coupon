219b708ff963e3a691a251750d989494fa37e2d8
jihyeChu <09strawberry@naver.com>
2025-01-10 22:20:03 +0900

    [ feat ] 스케줄러 적용을 위한 설정 추가 (#18)

---
+++ src/main/java/com/qring/coupon/infrastructure/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package com.qring.coupon.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        taskScheduler.setThreadNamePrefix("scheduler-task-");
+        taskScheduler.initialize();
+        return taskScheduler;
+    }
+
+}

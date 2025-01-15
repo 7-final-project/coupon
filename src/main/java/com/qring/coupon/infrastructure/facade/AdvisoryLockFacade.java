5b81f6ea98a0b1294631dbda197de5a539fc888c
jihyeChu <09strawberry@naver.com>
2025-01-16 00:37:09 +0900

    [ feat ] PostgreSQL 을 사용한 쿠폰 발급 Advisory Lock 추가 : 2가지 버전 ( #17 )

--- src/main/java/com/qring/coupon/infrastructure/facade/AdvisoryLockFacade.java
+++ src/main/java/com/qring/coupon/infrastructure/facade/AdvisoryLockFacade.java
@@ -13,17 +13,26 @@ public class AdvisoryLockFacade {
     private final CouponServiceV1 couponServiceV1;
     private final AdvisoryLockRepository advisoryLockRepository;
 
+    // -----
+    // NOTE : pg__advisory_xact_lock
     @Transactional
-    public CouponPostByIdResDTOV1 issueCouponWithLockById(Long userId, Long id, String username) {
-        if (!advisoryLockRepository.getLockByKey(id)) {
-            throw new RuntimeException("락 획득 실패");
+    public CouponPostByIdResDTOV1 issueCouponWithLockById1(Long userId, Long id, String username) {
+        advisoryLockRepository.getLockByKey(id);
+
+        return couponServiceV1.issueBy(userId, id, username);
+    }
+
+    // -----
+    // NOTE : pg_try_advisory_xact_lock
+    @Transactional
+    public CouponPostByIdResDTOV1 issueCouponWithLockById2(Long userId, Long id, String username) {
+        int maxAttempts = 5;
+        long retryIntervalMillis = 1000L;
+
+        boolean lockAcquired = advisoryLockRepository.getLockWithTry(id, maxAttempts, retryIntervalMillis);
+        if (!lockAcquired) {
+            throw new RuntimeException("락 획득 실패: 키 " + id);
         }
-        CouponPostByIdResDTOV1 couponPostByIdResDTOV1 = null;
-        try {
-            couponPostByIdResDTOV1 = couponServiceV1.issueBy(userId, id, username);
-        } finally {
-            advisoryLockRepository.releaseLockByKey(id);
-        }
-        return couponPostByIdResDTOV1;
+        return couponServiceV1.issueBy(userId, id, username);
     }
 }

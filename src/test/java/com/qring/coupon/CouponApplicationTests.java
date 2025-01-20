cdb016aebb1be10a6d26777a2b216997aef4f3ba
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-21 00:43:36 +0900

    [ chore ] @SpringBootTest 주석 처리 ( #14 )

--- src/test/java/com/qring/coupon/CouponApplicationTests.java
+++ src/test/java/com/qring/coupon/CouponApplicationTests.java
@@ -1,9 +1,8 @@
 package com.qring.coupon;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CouponApplicationTests {
 
     @Test

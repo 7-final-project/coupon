7b1c2107acce40d4890b9118440a968d83cf93d3
jihyeChu <09strawberry@naver.com>
2025-01-24 17:57:37 +0900

    [ feat ] 카프카 쿠폰 발급 메시지 발행 ( #25 )

---
+++ src/main/java/com/qring/coupon/infrastructure/messaging/kafka/KafkaMessageProducerImplV1.java
@@ -0,0 +1,25 @@
+package com.qring.coupon.infrastructure.messaging.kafka;
+
+import com.qring.coupon.application.v2.messaging.KafkaMessageProducerV1;
+import com.qring.coupon.infrastructure.messaging.kafka.dto.IssueCouponMessageDTOV1;
+import com.qring.coupon.infrastructure.util.EventSerializer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaMessageProducerImplV1 implements KafkaMessageProducerV1 {
+
+    @Value("${spring.kafka.topic.issuance-coupon-event}")
+    private String issuanceCouponEventTopic;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void publishCouponIssuanceMessage(IssueCouponMessageDTOV1 message) {
+
+        kafkaTemplate.send(issuanceCouponEventTopic, EventSerializer.serialize(message));
+
+    }
+}
